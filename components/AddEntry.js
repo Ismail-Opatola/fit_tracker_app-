01774afa663437c171dcce80aa85f7437b85e9a8
Ismail Opatola <opatolamails@gmail.com>
2019-07-22 11:27:31 +0100

    add daily entry checker, text_button, reset method

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import Slider from "./Slider";
 import Steppers from "./Steppers";
 import DateHeader from "./DateHeader";
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -66,8 +68,31 @@ export default class AddEntry extends Component {
     // Save to 'DB'
     // Clear local notification
   };
+  reset = () => {
+    const key = timeToString()
+
+     // Update Redux
+
+     // Route to Home
+
+     // Update "DB"
+  }
   render() {
     const metaInfo = getMetricMetaInfo();
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name={'md-happy'}
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+          </View>
+      )
+    }
     return (
       <View>
         <DateHeader date={new Date().toLocaleDateString()} />
