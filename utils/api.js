424f0a4c895c55065aaa55b5aecf39a37627b412
Ismail Opatola <opatolamails@gmail.com>
2019-07-23 05:54:22 +0100

    ccreate asyncStorage api

---
+++ utils/api.js
@@ -0,0 +1,20 @@
+import { AsyncStorage } from "react-native";
+import { CALENDAR_STORAGE_KEY } from "./_calendar";
+
+// Submit a new entry for the day
+export function submitEntry({ key, entry }) {
+  // Add it to DB
+  return AsyncStorage.mergeItem(
+    CALENDAR_STORAGE_KEY,
+    JSON.stringify({ [key]: entry })
+  );
+}
+
+export function removeEntry(key) {
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    data[key] = undefined;
+    delete data[key];
+    AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data));
+  });
+}
