f6d2143423e6eda5d1d00bc8d925cad6def5713d
Ismail Opatola <opatolamails@gmail.com>
2019-07-26 18:00:14 +0100

    bug fix

--- components/MetricCard.js
+++ components/MetricCard.js
@@ -1,38 +1,36 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import DateHeader from "./DateHeader";
-import { getMetricMetaInfo } from "../utils/helpers";
-import { gray } from "../utils/colors";
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import DateHeader from './DateHeader'
+import { getMetricMetaInfo } from '../utils/helpers'
+import { gray } from '../utils/colors'
 
-export default function MetricCard({ date, metrics }) {
+export default function MetricCard ({ date, metrics }) {
   return (
     <View>
       {date && <DateHeader date={date} />}
-      {Object.keys(metrics).map(metric => {
-        const {
-          getIcon,
-          displayName,
-          unit,
-          backgroundColor
-        } = getMetricMetaInfo(metric);
-
+      {Object.keys(metrics).map((metric) => {
+        const { getIcon, displayName, unit, backgroundColor } = getMetricMetaInfo(metric)
         return (
-          <View style={StyleSheet.metric} key={metric}>
+          <View style={styles.metric} key={metric}>
             {getIcon()}
-            <View> metric component
-              <Text style={{ fontSize: 20 }}>{displayName}</Text>
-              <Text style={{ fontSize: 16, color: gray }}>{metrics[metric]} {unit}</Text>
+            <View>
+              <Text style={{fontSize: 20}}>
+                {displayName}
+              </Text>
+              <Text style={{fontSize: 16, color: gray}}>
+                {metrics[metric]} {unit}
+              </Text>
             </View>
           </View>
-        );
+        )
       })}
     </View>
-  );
+  )
 }
 
-const stlyes = StyleSheet.create({
+const styles = StyleSheet.create({
   metric: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: 12
-  }
-});
+  },
+})
