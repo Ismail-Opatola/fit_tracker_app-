ac1ef52f23a74e2d109272049470ca3b8ce63639
Ismail Opatola <opatolamails@gmail.com>
2019-07-26 16:31:53 +0100

    add custom-status-bar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import AddEntry from "./components/AddEntry";
 import History from "./components/History";
 import { createStore } from "redux";
@@ -8,6 +8,15 @@ import reducer from "./reducers";
 import { createBottomTabNavigator, createAppContainer, createNavigator, createMaterialTopTabNavigator} from 'react-navigation'
 import {purple, white} from './utils/colors';
 import { FontAwesome, Ionicons} from '@expo/vector-icons';
+import { Constants } from "expo";
+
+function CustomStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+} 
 
 const Tabs = {
   History: {
@@ -63,7 +72,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <View style={{height: 20}}/>
+          <CustomStatusBar backgroundColor={purple} barStyle='light-content'/>
           <AppContainer/>
         </View>
       </Provider>
